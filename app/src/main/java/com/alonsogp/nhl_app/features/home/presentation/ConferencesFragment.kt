3dd1f3a7de94802e0233ff09bc25aac16ee4d5a5
alonsogallego7 <alonso.galpac@educa.jcyl.es>
2023-04-06 21:46:18 +0200

    working on conferences fragment

--- app/src/main/java/com/alonsogp/nhl_app/features/home/presentation/ConferencesFragment.kt
+++ app/src/main/java/com/alonsogp/nhl_app/features/home/presentation/ConferencesFragment.kt
@@ -8,6 +8,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.alonsogp.nhl_app.R
 import com.alonsogp.nhl_app.databinding.FragmentConferencesBinding
 import com.faltenreich.skeletonlayout.Skeleton
 import dagger.hilt.android.AndroidEntryPoint
@@ -15,7 +18,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class ConferencesFragment : Fragment() {
 
-    private var conferencesFragmentBinding: FragmentConferencesBinding? = null
+    private var binding: FragmentConferencesBinding? = null
     private val viewModel by viewModels<ConferencesViewModel>()
     private var skeleton: Skeleton? = null
 
@@ -23,14 +26,30 @@ class ConferencesFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        conferencesFragmentBinding = FragmentConferencesBinding.inflate(inflater)
-        return conferencesFragmentBinding?.root
+        binding = FragmentConferencesBinding.inflate(inflater)
+        setupView()
+        return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupObservers()
         viewModel.getConferences()
+    }
+
+    private fun setupView() {
+        binding?.apply {
+            layoutToolbar.sectionToolbar.apply {
+                title = "Teams & Players"
+                setNavigationOnClickListener {
+                    findNavController().navigateUp()
+                }
+            }
+        }
+    }
+
+    private fun bind() {
+        //TODO
     }
 
     private fun setupObservers() {
@@ -45,5 +64,11 @@ class ConferencesFragment : Fragment() {
                 }
             }
         viewModel.uiState.observe(viewLifecycleOwner, newsFeedSubscriber)
+    }
+
+    private fun navigateToDivisions(conferenceId: Int) {
+        findNavController().navigate(
+            ConferencesFragmentDirections.actionToDivisionsFragment(conferenceId)
+        )
     }
 }
